489472f9cc3141efd3d3714035adf7aae1835d7a
Domenico Giambra <sviluppo@editt.it>
2018-01-31 12:09:59 +0100

    Type error fix

--- Config.php
+++ Config.php
@@ -8,7 +8,7 @@ class Config extends Module_Config
 	{
 		if (isset($data['install'])) {
 			if (isset($data['crea-tabella'])) {
-				return $this->model->_Db->query('CREATE TABLE `zk_dimensioni_colonne` (
+				$this->model->_Db->query('CREATE TABLE `zk_dimensioni_colonne` (
 				  `id` int(11) NOT NULL AUTO_INCREMENT,
 				  `table` varchar(250) COLLATE utf8_unicode_ci NOT NULL DEFAULT \'admin_users\',
 				  `utente` int(11) NOT NULL,
@@ -18,6 +18,7 @@ class Config extends Module_Config
 				  PRIMARY KEY (`id`),
 				  KEY `dimensioni_colonne_utente_idx` (`utente`)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
+				return true;
 			} else {
 				return true;
 			}
