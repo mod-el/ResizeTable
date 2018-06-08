3cbc5833c026fbf757aab987cc11b1e2653ffe2d
Domenico Giambra <zorinik@gmail.com>
2018-06-08 20:48:28 +0200

    Installation table is now automatically created if needed

--- Config.php
+++ Config.php
@@ -6,9 +6,7 @@ class Config extends Module_Config
 {
 	public function install(array $data = []): bool
 	{
-		if (isset($data['install'])) {
-			if (isset($data['crea-tabella'])) {
-				$this->model->_Db->query('CREATE TABLE `zk_dimensioni_colonne` (
+		$this->model->_Db->query('CREATE TABLE IF NOT EXISTS `zk_dimensioni_colonne` (
 				  `id` int(11) NOT NULL AUTO_INCREMENT,
 				  `table` varchar(250) COLLATE utf8_unicode_ci NOT NULL DEFAULT \'admin_users\',
 				  `utente` int(11) NOT NULL,
@@ -18,23 +16,6 @@ class Config extends Module_Config
 				  PRIMARY KEY (`id`),
 				  KEY `dimensioni_colonne_utente_idx` (`utente`)
 				) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;');
-				return true;
-			} else {
-				return true;
-			}
-		}
-
 		return true;
-	}
-
-	/**
-	 * Returns the config template
-	 *
-	 * @param array $request
-	 * @return string
-	 */
-	public function getTemplate(array $request)
-	{
-		return 'install';
 	}
 }
